bf6b6e346c5e37175ef69ca08573bd1688e8d42e
azrahel98 <33237844+azrahel98@users.noreply.github.com>
2026-02-12 12:17:11 -0500

    Comment out solicitarPermisoHost in main.vue
    Comment out the solicitarPermisoHost call in onMounted.

--- src/views/main.vue
+++ src/views/main.vue
@@ -7,7 +7,7 @@ import { useClientStore } from '@store/cliente'
 onMounted(async () => {
   const clientStore = useClientStore()
   await clientStore.initSocket()
-  await clientStore.solicitarPermisoHost()
+  //await clientStore.solicitarPermisoHost()
 })
 </script>
 
